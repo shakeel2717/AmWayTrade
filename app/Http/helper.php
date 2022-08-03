7ce77642354acf22829badf0651cf41c6b24dc9b
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-03 08:56:34 -0700

    user can see downline teams

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -1,6 +1,20 @@
 <?php
 
+use App\Models\User;
+
 function balance($user_id)
 {
     return 0;
+}
+
+
+function allRefers($user_id)
+{
+    $allRefers = [];
+    $user = User::find($user_id);
+    $refers = User::where('refer', $user->username)->get();
+    foreach ($refers as $refer) {
+        $allRefers[] = $refer->id;
+    }
+    return collect($allRefers);
 }
