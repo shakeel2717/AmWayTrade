892670a67b4848f21088a285ac878a995888bacd
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-04 04:53:31 -0700

    admin dashboard update

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Models\UserPlan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -14,7 +17,10 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view("admin/dashboard/index");
+        $users = User::get();
+        $userPlans = UserPlan::get();
+        $transactions = Transaction::get();
+        return view("admin/dashboard/index", compact("users", "userPlans", "transactions"));
     }
 
     /**
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="row">
     <div class="intro-y col-md-3">
-        <div class="box p-5 zoom-in">
+        <div class="box p-5 zoom-in mt-4">
             <div class="d-flex align-items-center">
                 <div class="w-2/4 flex-none">
                     <div class="fs-lg fw-medium truncate">
-                        {{ env('APP_CURRENCY') }}{{ number_format(balance(auth()->user()->id), 2) }}
+                        {{ $users->count() }}
                     </div>
-                    <div class="text-gray-600 lead mt-1">Available Balance</div>
+                    <div class="text-gray-600 lead mt-1">Total Users</div>
                 </div>
                 <div class="flex-none ms-auto position-relative">
                     <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
@@ -18,46 +18,226 @@
         </div>
     </div>
     <div class="intro-y col-md-3">
-        <div class="box p-5 zoom-in">
+        <div class="box p-5 zoom-in mt-4">
             <div class="d-flex align-items-center">
                 <div class="w-2/4 flex-none">
                     <div class="fs-lg fw-medium truncate">
-                        {{ env('APP_CURRENCY') }}{{ number_format(totalInvest(auth()->user()->id), 2) }}
+                        {{ $users->where('status', false)->count() }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Pending Users</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        {{ $users->where('status', true)->count() }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Active Users</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        {{ $users->where('suspend', true)->count() }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Suspended Users</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($userPlans->sum("amount"),2) }}
                     </div>
                     <div class="text-gray-600 lead mt-1">Total Investment</div>
                 </div>
                 <div class="flex-none ms-auto position-relative">
-                    <img src="{{ asset('assets/icons/tasks.png') }}" alt="Balance">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
                 </div>
             </div>
         </div>
     </div>
     <div class="intro-y col-md-3">
-        <div class="box p-5 zoom-in">
+        <div class="box p-5 zoom-in mt-4">
             <div class="d-flex align-items-center">
                 <div class="w-2/4 flex-none">
                     <div class="fs-lg fw-medium truncate">
-                        {{ env('APP_CURRENCY') }}{{ number_format(totalPayout(auth()->user()->id), 2) }}
+                        ${{ number_format($userPlans->where('status',false)->sum("amount"),2) }}
                     </div>
-                    <div class="text-gray-600 lead mt-1">Total Payout</div>
+                    <div class="text-gray-600 lead mt-1">Pending Investment</div>
                 </div>
                 <div class="flex-none ms-auto position-relative">
-                    <img src="{{ asset('assets/icons/directory.png') }}" alt="Balance">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
                 </div>
             </div>
         </div>
     </div>
     <div class="intro-y col-md-3">
-        <div class="box p-5 zoom-in">
+        <div class="box p-5 zoom-in mt-4">
             <div class="d-flex align-items-center">
                 <div class="w-2/4 flex-none">
                     <div class="fs-lg fw-medium truncate">
-                        {{ allRefers(auth()->user()->id)->count() }}
+                        ${{ number_format($userPlans->where('status',true)->sum("amount"),2) }}
                     </div>
-                    <div class="text-gray-600 lead mt-1">My Downline Team</div>
+                    <div class="text-gray-600 lead mt-1">Active Investment</div>
                 </div>
                 <div class="flex-none ms-auto position-relative">
-                    <img src="{{ asset('assets/icons/hierarchy.png') }}" alt="Balance">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($userPlans->where('expire',true)->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Expired Investment</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','withdraw')->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Total Withdrawals</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','withdraw')->where('status',false)->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Pending Withdrawals</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','withdraw')->where('status',true)->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Approved Withdraw</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','withdraw')->where('status',true)->where('created_at', '>=', date('Y-m-d'))->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Today Withdraw</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','deposit')->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Total Deposit</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','deposit')->where('status',false)->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Pending Deposit</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','deposit')->where('status',true)->sum("amount"),2) }}
+                    </div>
+                    <div class="text-gray-600 lead mt-1">Active Deposit</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="intro-y col-md-3">
+        <div class="box p-5 zoom-in mt-4">
+            <div class="d-flex align-items-center">
+                <div class="w-2/4 flex-none">
+                    <div class="fs-lg fw-medium truncate">
+                        ${{ number_format($transactions->where('type','deposit')->where('status',true)->where('reference','coinPayment')->sum("amount"),2) }}
+                    </div>
+                    <div class="text-theme-1 lead mt-1">CoinPayment Deposit</div>
+                </div>
+                <div class="flex-none ms-auto position-relative">
+                    <img src="{{ asset('assets/icons/graph.png') }}" alt="Balance">
                 </div>
             </div>
         </div>
