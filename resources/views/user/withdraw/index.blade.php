107464185ba09bb12415072152692bf63032a642
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-04 23:48:20 -0700

    Withdarw icon in withdarw section

--- resources/views/user/withdraw/index.blade.php
+++ resources/views/user/withdraw/index.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.dashboard')
 @section('title', 'Withdraw Funds')
 @section('content')
-<div class="row mt-5">
-    <div class="intro-y col-md-8 mx-auto">
-        <div class="intro-y box">
-            <div class="d-flex flex-column flex-sm-row align-items-center p-5 border-bottom border-gray-200 dark-border-dark-5">
-                <h2 class="fw-medium fs-base me-auto">
-                    @yield('title')
-                </h2>
-            </div>
-            <div class="card-body">
-                @livewire("user.withdraw-card")
+    <div class="row mt-5">
+        <div class="intro-y col-md-8 mx-auto">
+            <div class="intro-y box">
+                <div
+                    class="d-flex flex-column flex-sm-row align-items-center p-5 border-bottom border-gray-200 dark-border-dark-5">
+                    <h2 class="fw-medium fs-base me-auto">
+                        @yield('title')
+                    </h2>
+                </div>
+                <div class="card-body">
+                    <div class="text-center mb-4">
+                        <img src="{{ asset('assets/icons/withdraw.png') }}" alt="Withdarw" class="img-fluid">
+                    </div>
+                    @livewire('user.withdraw-card')
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
