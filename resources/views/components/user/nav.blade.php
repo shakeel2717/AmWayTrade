8598a6b8f471d92a41bc62296e17fc888f69b6eb
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-08 18:12:17 +0500

    my account posotionj update

--- resources/views/components/user/nav.blade.php
+++ resources/views/components/user/nav.blade.php
@@ -6,6 +6,33 @@
 </li>
 
 <li>
+    <a href="javascript:;" class="{{ $mode == true ? 'side-' : '' }}menu">
+        <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="sliders"></i> </div>
+        <div class="{{ $mode == true ? 'side-' : '' }}menu__title">
+            My Account
+            <div class="{{ $mode == true ? 'side-' : '' }}menu__sub-icon"> <i data-feather="chevron-down"></i>
+            </div>
+        </div>
+    </a>
+    <ul class="{{ $mode == true ? 'side-' : '' }}menu__sub">
+        <li>
+            <a href="{{ route('user.profile.index') }}" class="{{ $mode == true ? 'side-' : '' }}menu">
+                <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="corner-down-right"></i>
+                </div>
+                <div class="{{ $mode == true ? 'side-' : '' }}menu__title"> My Profile </div>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('user.profile.change.password') }}" class="{{ $mode == true ? 'side-' : '' }}menu">
+                <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="corner-down-right"></i>
+                </div>
+                <div class="{{ $mode == true ? 'side-' : '' }}menu__title"> Change Password </div>
+            </a>
+        </li>
+    </ul>
+</li>
+
+<li>
     <a href="{{ route('user.deposit.index') }}" class="{{ $mode == true ? 'side-' : '' }}menu">
         <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="shopping-bag"></i> </div>
         <div class="{{ $mode == true ? 'side-' : '' }}menu__title"> Fund Deposit </div>
@@ -151,33 +178,6 @@
     </ul>
 </li>
 
-
-<li>
-    <a href="javascript:;" class="{{ $mode == true ? 'side-' : '' }}menu">
-        <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="sliders"></i> </div>
-        <div class="{{ $mode == true ? 'side-' : '' }}menu__title">
-            My Account
-            <div class="{{ $mode == true ? 'side-' : '' }}menu__sub-icon"> <i data-feather="chevron-down"></i>
-            </div>
-        </div>
-    </a>
-    <ul class="{{ $mode == true ? 'side-' : '' }}menu__sub">
-        <li>
-            <a href="{{ route('user.profile.index') }}" class="{{ $mode == true ? 'side-' : '' }}menu">
-                <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="corner-down-right"></i>
-                </div>
-                <div class="{{ $mode == true ? 'side-' : '' }}menu__title"> My Profile </div>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('user.profile.change.password') }}" class="{{ $mode == true ? 'side-' : '' }}menu">
-                <div class="{{ $mode == true ? 'side-' : '' }}menu__icon"> <i data-feather="corner-down-right"></i>
-                </div>
-                <div class="{{ $mode == true ? 'side-' : '' }}menu__title"> Change Password </div>
-            </a>
-        </li>
-    </ul>
-</li>
 
 <li>
     <a href="{{ route('user.kyc.index') }}" class="{{ $mode == true ? 'side-' : '' }}menu">
