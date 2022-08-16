cedd63cb0d7a501400932ef2978f9fe4dd101cfc
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-16 14:33:42 +0500

    add blaance update for log

--- app/Http/Livewire/User/AddBalance.php
+++ app/Http/Livewire/User/AddBalance.php
@@ -28,7 +28,9 @@ class AddBalance extends Component
         $address = Address::where('user_id', auth()->user()->id)->where('currency_id', $coin)->first();
         if ($address != "") {
             $this->qr = $address->address;
+            info("Already Address Found");
         } else {
+            info("Already Not Address Found");
             // fetching the address from coinPayment
             $private_key = env('PRIKEY');
             $public_key = env('PUBKEY');
@@ -40,12 +42,14 @@ class AddBalance extends Component
                 $buyer_email = auth()->user()->email;
                 $ipn_url = env('IPN_URL');
                 $information = $cps_api->GetCallbackAddressWithIpn($currency2, $ipn_url, $buyer_email);
+                info("Try Successs");
             } catch (Exception $e) {
                 echo 'Error: ' . $e->getMessage();
                 exit();
             }
 
             if ($information['error'] != 'ok') {
+                info("Error");
                 return "<h3>There's Some Issue in The API, Please Contact Support!</h3>";
             }
 
@@ -54,6 +58,7 @@ class AddBalance extends Component
             $address->currency_id = $coin;
             $address->address = $information['result']['address'];
             $address->save();
+            info("Address Saved");
 
             $this->qr = $address->address;
         }
