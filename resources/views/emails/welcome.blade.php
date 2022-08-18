67e620392f8c3cf9285ba05b3151d8335e032aef
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-18 16:51:50 +0500

    username -> name in welcoem email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Welcome to {{ env('APP_NAME') }}
 
-Dear {{ auth()->user()->username }} Congratulations!
+Dear {{ auth()->user()->name }} Congratulations!
 We welcome you to become a part of the {{ env('APP_NAME') }} and excel in your growth through tremendous opportunities at our platform.
 
 @component('mail::button', ['url' => route('user.dashboard.index')])
