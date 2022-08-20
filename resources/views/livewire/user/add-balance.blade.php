aeb64af4b577ec75239eb6aca9931fc96afb21ef
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-20 19:26:25 +0500

    deposit page update

--- resources/views/livewire/user/add-balance.blade.php
+++ resources/views/livewire/user/add-balance.blade.php
@@ -3,7 +3,7 @@
         <div>
             <label>Select Payment Currency</label>
             <div class="mt-2">
-                <select class="form-control w-full" wire:model="coin" name="coin" data-placeholder="Select your Currency">
+                <select class="form-control w-full" wire:model="coin" name="coin" data-placeholder="Select your Currency" @if ($icon != '') disabled @endif>
                     <option>Select Your Currency</option>
                     @forelse ($currencies as $currency)
                     <option value="{{ $currency->id }}">{{ $currency->name }}</option>
@@ -19,7 +19,7 @@
             </div>
             <div class="mt-2">
                 <option for="amount">Enter Amount in USD</option>
-                <input type="text" wire:model="amount" class="form-control" name="amount" placeholder="Amount" id="amount">
+                <input type="text" wire:model="amount" class="form-control" name="amount" placeholder="Amount" id="amount" @if ($icon != '') disabled @endif>
                 @error('amount')
                 <div class="mt-1 error text-theme-11">
                     {{ $message }}
@@ -51,7 +51,7 @@
         @if ($icon != '')
         <div class="d-flex justify-content-center align-items-center">
             <div class="code">
-                <p class="">Only Send <span class="fw-bold">{{ $coin }}</span> to this Address, Otherwise your Asset will be lost.</p>
+                <p class="">Send <span class="fw-bold">{{ number_format($amount,2) }} of {{ $coin }} to the following address</span>  Only Send <span class="fw-bold">{{ $coin }}</span> to this Address, Otherwise your Asset will be lost.</p>
                 <img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={{ $qr }}&choe=UTF-8" alt="QR Address" class="img-fluid mx-auto" width="250">
             </div>
         </div>
