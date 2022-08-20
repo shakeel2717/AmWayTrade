83d023b0028bfc72bb4b17a10e5efb1ec660c5de
Shakeel Ahmad <shakeel2717@gmail.com>
2022-08-20 23:24:27 +0500

    all depsoit update

--- resources/views/user/history/deposit.blade.php
+++ resources/views/user/history/deposit.blade.php
@@ -16,12 +16,3 @@
     </div>
 </div>
 @section('footer')
-<script>
-    function copyFunction() {
-        var copyText = document.getElementById("link");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /* For mobile devices */
-        navigator.clipboard.writeText(copyText.value);
-    }
-</script>
-@endsection
